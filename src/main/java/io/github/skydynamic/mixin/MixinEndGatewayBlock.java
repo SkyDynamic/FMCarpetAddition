f7828f64cbe115e363f02b05d094fba47350c96c
AsOne2020 <59190831+AsOne2020@users.noreply.github.com>
2025-03-15 17:28:54 +0800

    Support for 1.21.4

--- src/main/java/io/github/skydynamic/mixin/MixinEndGatewayBlock.java
+++ src/main/java/io/github/skydynamic/mixin/MixinEndGatewayBlock.java
@@ -4,49 +4,47 @@ import io.github.skydynamic.FuckMojangCarpetAdditionSettings;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Relative;
+import net.minecraft.world.entity.projectile.ThrownEnderpearl;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.EndGatewayBlock;
 import net.minecraft.world.level.block.entity.TheEndGatewayBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.portal.DimensionTransition;
+import net.minecraft.world.level.portal.TeleportTransition;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.util.Set;
 
 @Mixin(EndGatewayBlock.class)
 public abstract class MixinEndGatewayBlock {
-    @Shadow
-    private static Vec3 calculateExitMovement(Entity entity) {
-        return null;
-    }
-
     @Inject(
-        method = "getPortalDestination",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/world/level/portal/DimensionTransition;<init>(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;FFLnet/minecraft/world/level/portal/DimensionTransition$PostDimensionTransition;)V",
-            shift = At.Shift.BEFORE
-        ),
-        cancellable = true
+            method = "getPortalDestination",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/portal/TeleportTransition;<init>(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;FFLjava/util/Set;Lnet/minecraft/world/level/portal/TeleportTransition$PostTeleportTransition;)V",
+                    shift = At.Shift.BEFORE
+            ),
+            cancellable = true
     )
     private void injectGetPortalDestination(
-        ServerLevel serverLevel,
-        Entity entity,
-        BlockPos blockPos,
-        CallbackInfoReturnable<DimensionTransition> cir
+            ServerLevel serverLevel,
+            Entity entity,
+            BlockPos blockPos,
+            CallbackInfoReturnable<TeleportTransition> cir
     ) {
         TheEndGatewayBlockEntity blockEntity = (TheEndGatewayBlockEntity) serverLevel.getBlockEntity(blockPos);
         Vec3 vec3 = blockEntity.getPortalPosition(serverLevel, blockPos);
-        DimensionTransition dimensionTransition = new DimensionTransition(
-            serverLevel,
-            vec3,
-            calculateExitMovement(entity),
-            entity.getYRot(),
-            entity.getXRot(),
-            DimensionTransition.DO_NOTHING
+        TeleportTransition dimensionTransition = new TeleportTransition(
+                serverLevel,
+                vec3,
+                Vec3.ZERO,
+                0.0F,
+                0.0F,
+                entity instanceof ThrownEnderpearl ? Set.of() : Relative.union(new Set[]{Relative.DELTA, Relative.ROTATION}),
+                TeleportTransition.DO_NOTHING
         );
         if (FuckMojangCarpetAdditionSettings.endGatewayDoNotAddLoadTicket.equals("all")) {
             cir.setReturnValue(dimensionTransition);
